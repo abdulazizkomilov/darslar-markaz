a291b29493b82ff33e357188d1e70a2896de3bab
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-01-20 04:12:00 +0500

    registration errors

--- 6_dars_dj/registration/views.py
+++ 6_dars_dj/registration/views.py
@@ -16,4 +16,4 @@ class BlogDetailView(DetailView):
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
