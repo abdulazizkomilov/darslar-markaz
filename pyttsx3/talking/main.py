348129e23607571df792ea27b1f8326e63aad3ef
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-01-31 14:32:08 +0500

    Update main.py

--- pyttsx3/talking/main.py
+++ pyttsx3/talking/main.py
@@ -31,42 +31,42 @@ def record_audio(ask=False):
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            speak('I did not get that')
+            print('<<<')
         except sr.RequestError:
             speak('Sorry, the service is down')
-        print(f">> {voice_data.lower()}")
+        print(f"<<< {voice_data.lower()}")
         return voice_data.lower()
 
 def speak(audio_string):
     tts = gTTS(text=audio_string, lang='en')
-    r = random.randint(1,20000000)
+    r = random.randint(1, 20000000)
     audio_file = 'audio' + str(r) + '.mp3'
-    tts.save(audio_file) # save as mp3
+    tts.save(audio_file)
     playsound.playsound(audio_file)
-    print(f"kiri: {audio_string}")
+    print(f">>> {audio_string}")
     os.remove(audio_file)
 
 def respond(voice_data):
-    if there_exists(['hey','hi','hello']):
+    if there_exists(['hey', 'hi', 'hello']):
         greetings = [f"hey, how can I help you {person_obj.name}", f"hey, what's up? {person_obj.name}", f"I'm listening {person_obj.name}", f"how can I help you? {person_obj.name}", f"hello {person_obj.name}"]
-        greet = greetings[random.randint(0,len(greetings)-1)]
+        greet = greetings[random.randint(0, len(greetings)-1)]
         speak(greet)
 
-    if there_exists(["what is your name","what's your name","tell me your name"]):
+    if there_exists(["what is your name", "what's your name", "tell me your name"]):
         if person_obj.name:
-            speak("my name is Alexis")
+            speak("my name is Sofiya")
         else:
-            speak("my name is Alexis. what's your name?")
+            speak("my name is Sofiya. what's your name?")
 
     if there_exists(["my name is"]):
         person_name = voice_data.split("is")[-1].strip()
         speak(f"okay, i will remember that {person_name}")
         person_obj.setName(person_name)
 
-    if there_exists(["how are you","how are you doing"]):
+    if there_exists(["how are you", "how are you doing"]):
         speak(f"I'm very well, thanks for asking {person_obj.name}")
 
-    if there_exists(["what's the time","tell me the time","what time is it"]):
+    if there_exists(["what's the time", "tell me the time", "what time is it"]):
         time = ctime().split(" ")[3].split(":")[0:2]
         if time[0] == "00":
             hours = '12'
@@ -89,13 +89,13 @@ def respond(voice_data):
         speak(f'Here is what I found for {search_term} on youtube')
 
     if there_exists(["price of"]):
-        search_term = voice_data.lower().split(" of ")[-1].strip() #strip removes whitespace after/before a term in string
+        search_term = voice_data.lower().split(" of ")[-1].strip()
         stocks = {
-            "apple":"AAPL",
-            "microsoft":"MSFT",
-            "facebook":"FB",
-            "tesla":"TSLA",
-            "bitcoin":"BTC-USD"
+            "apple": "AAPL",
+            "microsoft": "MSFT",
+            "facebook": "FB",
+            "tesla": "TSLA",
+            "bitcoin": "BTC-USD"
         }
         try:
             stock = stocks[search_term]
@@ -113,6 +113,6 @@ def respond(voice_data):
 time.sleep(1)
 
 person_obj = person()
-while(1):
+while 1:
     voice_data = record_audio()
     respond(voice_data)
