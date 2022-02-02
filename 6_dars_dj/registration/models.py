0fa00167dfbab357e3b61e19dd367fee501a5ce9
Abdulaziz <80712573+abdulazizkomilov@users.noreply.github.com>
2022-02-02 11:51:11 +0500

    age Positive Integer Field

--- 6_dars_dj/registration/models.py
+++ 6_dars_dj/registration/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 
 class Reclama(models.Model):
     title = models.CharField(max_length=100)
@@ -17,3 +18,10 @@ class Reclama(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.pk)])
+
+    
+    
+    
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    
