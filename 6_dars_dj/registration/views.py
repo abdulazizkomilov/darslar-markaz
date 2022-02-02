dc887a4e5aaf334cc1bffe27f7484adaeaa6e69a
Abdulaziz <80712573+abdulazizkomilov@users.noreply.github.com>
2022-02-02 11:55:56 +0500

    Create views.py

--- 6_dars_dj/registration/views.py
+++ 6_dars_dj/registration/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Reclama
@@ -14,6 +14,6 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
