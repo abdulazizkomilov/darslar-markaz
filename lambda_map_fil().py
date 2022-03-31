f58dab808416ecca2ca5ca04303b30495338e70d
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-03-31 14:21:15 +0500

    adds

--- lambda_map_fil().py
+++ lambda_map_fil().py
@@ -80,38 +80,3 @@ print(uzunlik(math.pi,10))
 
 # sonlar = r.sample(range(100),10)
 # juft = [son for son in sonlar if son%2==0]; print(juft)
-
-
-# f1 = lambda x: x * 10
-# print(f1(10))
-
-# f2 = lambda x, y: x * y
-# print(f2(5, 4))
-
-# from random import sample
-# from math import sqrt
-
-# x = list(range(0, 1001))
-# y = sample(x, k=10)
-# print(y)
-
-# ildizlar = list(map(lambda n: sqrt(n), y))
-# print(ildizlar)
-
-# toq_sonlar = list(filter(lambda n: n % 2, y))
-# print(toq_sonlar)
-
-
-# def tubmi(x):
-#     if x % 2 == 0 or x < 2:
-#         return False  # Son juft yoki 2 dan kichik bo'lsa
-#     if x == 2 or x == 3:
-#         return True  # Son 2 yoki 3 bo'lsa
-#     for i in range(3, x, 2):
-#         if x % i == 0:
-#             return False
-#     return True
-
-
-# tub_sonlar = list(filter(tubmi, range(100)))
-# print(tub_sonlar)
