7c237eb40748902c86e8fdd026b66cdd44ce4a93
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-04-02 12:23:59 +0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,30 +1,66 @@
-# import avto_info_mod # avto_info_mod faylini (modulini) chaqiramiz
-
-# avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
-# avto_info_mod.info_print(avto1)
+import random
 
 
+def sontop(x=10):
+    tasodifiy_son = random.randint(1, x)
+    print(f"Men 1 dan {x} gacha son o'yladim. Topa olasizmi?", end="")
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        taxmin = int(input(">>>"))
+        if taxmin < tasodifiy_son:
+            print("Kattaroq son ayting:", end="")
+        elif taxmin > tasodifiy_son:
+            print("Kichikroq son ayting:", end="")
+        else:
+            print("Yutdingiz!")
+            break
 
-# import avto_info_mod as aim # avto_info_mod ni qisqa nom aim bilan chaqiramiz
-
-# avto1 = aim.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
-# aim.info_print(avto1)
+    print(f"Tabriklayman. {taxminlar} ta taxmin bilan topdingiz!")
+    return taxminlar
 
 
-# from avto_info_mod import avto_info, info_print
+def sontop_pc(x=10):
+    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman.")
+    quyi = 1
+    yuqori = x
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        if quyi != yuqori:
+            taxmin = random.randint(quyi, yuqori)
+        else:
+            taxmin = quyi
+        javob = input(
+            f"Siz {taxmin} sonini o'yladingiz: to'g'ri (t),"
+            f"men o'ylagan son bundan kattaroq (+), yoki kichikroq (-)".lower()
+        )
+        if javob == "-":
+            yuqori = taxmin - 1
+        elif javob == "+":
+            quyi = taxmin + 1
+        else:
+            break
+    print(f"Men {taxminlar} ta taxmin bilan topdim!")
+    return taxminlar
 
-# avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
-# info_print(avto1)
+
+def play(x=10):
+    yana = True
+    while yana:
+        taxminlar_pc = sontop_pc(x)
+        taxminlar_user = sontop(x)
+
+        if taxminlar_user > taxminlar_pc:
+            print(f"Men {taxminlar_pc} taxmin bilan topdim va  yutdim!")
+        elif taxminlar_user < taxminlar_pc:
+            print(f"Siz {taxminlar_user} taxmin bilan topdingiz va yutdingiz!")
+        else:
+            print("Durrang!")
+        yana = int(input("Yana o'ynaymizmi? Ha(1)/Yo'q(0):"))
 
 
-# from avto_info_mod import avto_info as ainfo, info_print as iprint
-
-# avto1 = ainfo("GM", "Malibu", "Qora", "avtomat", 2020,40000)
-# iprint(avto1)
+play()
 
 
-
-# from avto_info_mod import *
-
-# avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
-# info_print(avto1)
+# son_top()
