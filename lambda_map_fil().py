c407e28ffa4f9b067e82aaa642359d45200ec01e
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-04-02 12:22:11 +0500

    adds

--- lambda_map_fil().py
+++ lambda_map_fil().py
@@ -1,13 +1,14 @@
 import math
 
-# def nom(argument):
+# def nom(x):
+#     ifoda = x*x
 #     return ifoda
 
-a = lambda x: x*x
-print(a(8))
+# a = lambda x: x*x
 
-uzunlik = lambda pi, r : 2*pi*r
-print(uzunlik(math.pi,10))
+
+# uzunlik = lambda pi, r : 2*pi*r
+# print(uzunlik(math.pi, 5))
 
 
 # def kbvadrat(x, y):
@@ -22,11 +23,18 @@ print(uzunlik(math.pi,10))
 
 # kvadrat = daraja(2)
 # kub = daraja(3)
-# print(f"3-ning kvadrati {kvadrat(3)} ga, " f"kubi {kub(3)} ga teng")
+
+# yuz = daraja(100)
+# print(yuz(1000))
+
+# # print(kvadrat(38))
+# besh_dj = daraja(5)
+# print(besh_dj(38))
+# # print(f"3-ning kvadrati {kvadrat(3)} ga, " f"kubi {kub(3)} ga teng")
 
 
 
-# from math import sqrt  # sqrt - kvadrat ildiz
+from math import sqrt  # sqrt - kvadrat ildiz
 
 # sonlar = list(range(11))  # 0 dan 10 gacha sonlar ro'yxati
 # ildizlar = list(map(sqrt,sonlar))
@@ -50,10 +58,10 @@ print(uzunlik(math.pi,10))
 # ismlar = ['hasan','husan','olim','umid']
 # print(list(map(lambda matn:matn.upper(),ismlar)))
 
-# import random as r
+import random as r
 
-# sonlar = r.sample(range(100), 10)  # 0-99 oralig'ida 10 ta tasodifiy sonlar
-# print(sonlar)
+sonlar = r.sample(range(100), 10)  # 0-99 oralig'ida 10 ta tasodifiy sonlar
+print(sonlar)
 
 
 # def juftmi(x):
@@ -64,8 +72,8 @@ print(uzunlik(math.pi,10))
 # juft_sonlar = list(filter(juftmi,sonlar))
 # print(juft_sonlar)
 
-# juft_sonlar = list(filter(lambda x: x % 2 == 0, sonlar))
-# print(juft_sonlar)
+juft_sonlar = list(filter(lambda x: x % 2 == 0, sonlar))
+print(juft_sonlar)
 
 
 # mevalar = ["olma", "anor", "anjir", "shaftoli", "o'rik", "tarvuz", "qovun", "banan"]
@@ -80,3 +88,29 @@ print(uzunlik(math.pi,10))
 
 # sonlar = r.sample(range(100),10)
 # juft = [son for son in sonlar if son%2==0]; print(juft)
+
+
+
+# import random as r # random modulini r deb chaqirayapmiz
+
+# son = r.randint(0,100) # 0 va 100 oralig'ida tasodifiy son
+# print(son)
+
+
+# ismlar = ['olim','anvar','hasan','husan']
+# ism = r.choice(ismlar) # ismlar dan tasodifiy ism tanlaymiz
+# print(ism)
+# print(r.choice(ism)) # ismdan tasodifiy harf tanlaymiz
+
+
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+
+# x = list(range(11))   # x ichidagi elementlarni tasodifiy tartibda qaytaruvchi funksiya
+# print(x)
+# r.shuffle(x)
+# print(x)
+
+
