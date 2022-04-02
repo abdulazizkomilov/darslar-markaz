477df53c201193e019f4951c1b3de115d7ccdf0d
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-04-02 12:22:41 +0500

    Update 4_dars.py

--- 4_dars.py
+++ 4_dars.py
@@ -22,51 +22,54 @@ sonlar = [22, -58.2, 34.0, 67, 1983, 123_456_678_000, 112.4]
 # del sonlar[0]
 # print(sonlar)
 
-t_shaxslar = ["Amir Temur", "Imom Buxoriy", "Napoleon"]
+# t_shaxslar = ["Amir Temur", "Imom Buxoriy", "Napoleon"]
 
-z_shaxslar = ["Bill Gates", "Elon Musk", "Doasnald Trump"]
+# z_shaxslar = ["Bill Gates", "Elon Musk", "Doasnald Trump"]
 
-# # Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
-print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,\n\
-zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
-suhbat qilishni istar edim\n")
+# # # Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
+# print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,\n\
+# zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
+# suhbat qilishni istar edim\n")
 
-print(f"""Men tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,
-zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan
-suhbat qilishni istar edim""")
+# print(f"""Men tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,
+# zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan
+# suhbat qilishni istar edim""")
 
-print(z_shaxslar)
-print(t_shaxslar)
+# print(z_shaxslar)
+# print(t_shaxslar)
 
 # # # friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga 
 # # chaqirmoqchi bo'lgan do'stlaringizni kiriting.
-# friends = []
-# friends.append("John")
-# friends.append("Alex")
-# friends.append("Danny")
-# friends.append("Sobirjon")
-# friends.append("Vanya")
-# # print(friends)
+friends = []
+friends.append("John")
+friends.append("Alex")
+friends.append("Danny")
+friends.append("Sobirjon")
+friends.append("Vanya")
+# friends.append(20)
+# friends.append(982.43)
+# print(friends)
 
 # # # Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang.
-# # friends.remove("John")
-# # friends.remove("Alex")
-# # friends.remove("Vanya")
-# # print(friends)
+# friends.remove("John")
+# friends.remove("Alex")
+# friends.remove("Vanya")
+# print(friends)
 
 # # Ro'yxatning oxiriga, boshiga va o'rtasiga yangi ismlar qo'shing.
 # friends.append("Hasan")
 # friends.insert(0, "Husan")
 # friends.insert(2, "Ivan")
+# friends.insert(3, 90)
 # print(friends)
 
 
 # Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. .pop() va .append() metodlari yordamida mehmonga kelgan do'stlaringizning ismini friends ro'yxatidan sug'urib olib, mehmonlar ro'yxatiga qo'shing.
-# mehmonlar = []
-# mehmonlar.append(friends.pop(3))
-# mehmonlar.append(friends.pop(-1))
-# mehmonlar.append(friends.pop(0))
-# print("\nKelgan mehmonlar: ", mehmonlar)
+mehmonlar = []
+mehmonlar.append(friends.pop(3))
+mehmonlar.append(friends.pop(-1))
+mehmonlar.append(friends.pop(0))
+print("\nKelgan mehmonlar: ", mehmonlar)
 
 
 
