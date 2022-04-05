0af7c47fddaa48165a255fa13176395589cb4715
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-04-05 13:48:18 +0500

    adds

--- 7_dars.py
+++ 7_dars.py
@@ -1,5 +1,5 @@
 # for operatori
- .isdigit()
+
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 # for mehmon in mehmonlar:
 #     print(mehmon)
@@ -13,14 +13,12 @@
 #     print(f"Hurmatli {mehmon.capitalize()}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
 #     print("Hurmat bilan, Palonchiyevlar oilasi")
 
-# a = 0
-# b = 10
+# a = 1
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 # for mehmon in mehmonlar:
-#     # a = a + 1
-#     a += 1
-#     print(f"{a} Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
+#     print(f"{a}. Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
 #     print("Hurmat bilan, Palonchiyevlar oilasi\n")
+#     a += 1
 
 
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
@@ -31,9 +29,8 @@
 
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 # for mehmon in mehmonlar:
-#     print(mehmonlar)
+#     print(mehmon)
     
-#     print(mehmon) # bu qator tsikl tashqarisida bo'lishi kerak edi
     
     
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
@@ -46,8 +43,10 @@
 # sonlar = list(range(1,11))
 # for son in sonlar:
 #     print(f"{son} ning kvadrati {son**2} ga teng")
-    
-# sonlar = list(range(11)) # 1 dan 10 gacha sonlar ro'yxatini yaratamiz
+#     print(f"{son} ning kubi {son**3} ga teng\n")
+
+
+# sonlar = list(range(11)) # 0 dan 10 gacha sonlar ro'yxatini yaratamiz
 # sonlar_kvadrati = [] # bo'sh ro'yxat yaratamiz
 # for son in sonlar:  # sonlar dagi har bir son uchun
 #     sonlar_kvadrati.append(son**2) # uning kv.ni hisoblab, sonlar_kvadrati ga yuklaymiz
@@ -56,13 +55,16 @@
 # print(f"sonlar_kvadrati: {sonlar_kvadrati}")
 
 
-dostlar = [] # bo'sh ro'yxat
-print("5 ta eng yaqin do'stingiz kim?")
-for n in range(5): # n bu yerda 0 dan 4 gacha qiymatlar oladi
-    dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
-print(dostlar)
+
+# dostlar = [] # bo'sh ro'yxat
+# print("5 ta eng yaqin do'stingiz kim?")
+# for n in range(5): # n bu yerda 0 dan 4 gacha qiymatlar oladi
+#     dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
+# # print(dostlar)
 
 
-for n in sonlar:
-    for a in n:
-        prin
+# a_1 = [1, 3, 5]
+# b_1 = [2, 4, 6]
+# for a in a_1:
+#     for b in b_1:
+#         print(f"a + b; {a} + {b} = {a+b}")
