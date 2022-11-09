47b3fa6fff80d98b93dfe3c6e91b95a32a8a6359
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-11-09 17:37:05 +0500

    Update 16_dars.py

--- 16_dars.py
+++ 16_dars.py
@@ -1,5 +1,5 @@
 # def salom_ber():
-#     """Salom beruvchi funksiyani"""
+#     """Salom beruvchi funksiya"""
 #     print("Assalomu alaykum!")
 
 
@@ -11,15 +11,10 @@
 #     unga salom beruvchi funksiya"""
 #     print(f"Assalomu alaykum, hurmatli {ism.title()} {familya.title()}!")
 
-
-# #2
 # salom_ber("hasan", "olimov")
 # salom_ber("olim", "hakimov")
 
-# # # # salom_ber()
-
-
-#3
+# 3
 # def toliq_ism(ism, familiya):
 #     """Foydalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
 #     print(
@@ -29,7 +24,6 @@
 
 
 # toliq_ism('olim','hakimov')
-# toliq_ism('hakimov','olim')
 
 
 # def yosh_hisobla(ism, tugilgan_yil):
@@ -37,11 +31,11 @@
 #     print(f"{ism.title()} {2022-tugilgan_yil} yoshda")
 
 
-# yosh_hisobla('olim',1997)
+# yosh_hisobla('olim', 1997)
 # # yosh_hisobla(1997,'olim')
 
 # yosh_hisobla(tugilgan_yil=1997, ism="ali")
-# toliq_ism(familiya="hakimov", ism="olim")
+# # toliq_ism(familiya="hakimov", ism="olim")
 
 
 # #4
@@ -50,8 +44,9 @@
 #     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
 
 
-# yosh_hisobla(1995,2022)
-# yosh_hisobla(1993)
+# yosh_hisobla(2005,2022)
+# yosh_hisobla(1993, 2024)
 
 # tyil = int(input("Tug'ilgan yilingizni kiriting: "))
-# yosh_hisobla(tyil)
+# jyil = int(input("Joriy yilni kiriting: "))
+# yosh_hisobla(tyil, jyil)
