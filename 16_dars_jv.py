4783be0834459fc34ecf0f2262ffec6802d33797
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-11-09 17:37:18 +0500

    Create 16_dars_jv.py

---
+++ 16_dars_jv.py
@@ -0,0 +1,24 @@
+# 2
+# def son_kv_kub(son):
+#     """..."""
+#     print(son**2, son**3)
+
+
+# 3
+# def son_kv_kub(son):
+#     """..."""
+#     if son % 2 == 0:
+#         pass
+#     else:
+#         pass
+
+# 4
+# def son_kv_kub(son_1, son_2):
+#     """..."""
+#     if son_1 >= son_2:
+#         pass
+#     else:
+#         pass
+
+# 5
+
