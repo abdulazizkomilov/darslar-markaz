4336de6a4ebacccb5cf0ed4b18696fa680e7b5ef
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-11-13 20:10:04 +0500

    Update 17_dars.py

--- 17_dars.py
+++ 17_dars.py
@@ -23,8 +23,9 @@
 # print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
 
 
-# ****
-# def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+# # ****
+# def avto_info(kompaniya, model, rangi, korobka, yili, 
+#               narhi=None):
 #     avto = {
 #         "kompany": kompaniya,
 #         "model": model,
@@ -49,16 +50,16 @@
     
     
     
-# def oraliq(min, max):
-#     sonlar = []
-#     while min < max:
-#         sonlar.append(min)
-#         min += 1
-#     return sonlar
+def oraliq(min, max):
+    sonlar = []
+    while min < max:
+        sonlar.append(min)
+        min += 1
+    return sonlar
 
 
-# print(oraliq(0,10))
-# print(oraliq(10, 21))
+print(oraliq(0,10))
+print(oraliq(10, 21))
 
 # def oraliq(min,max,qadam=1):
 #     sonlar = []
@@ -71,42 +72,42 @@
 
 
 
-def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
-    avto = {
-        "kompaniya": kompaniya,
-        "model": model,
-        "rang": rangi,
-        "korobka": korobka,
-        "yil": yili,
-        "narh": narhi,
-    }
-    return avto
+# def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+#     avto = {
+#         "kompaniya": kompaniya,
+#         "model": model,
+#         "rang": rangi,
+#         "korobka": korobka,
+#         "yil": yili,
+#         "narh": narhi,
+#     }
+#     return avto
 
 
-print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
-avtolar = []  # salondagi avtolar uchun bo'sh ro'yxat
-while True:
-    print("\nQuyidagi ma'lumotlarni kiriting", end="")
-    kompaniya = input("Ishlab chiqaruvchi: ")
-    model = input("Modeli: ")
-    rangi = input("Rangi: ")
-    korobka = input("Korobka: ")
-    yili = input("Ishlab chiqarilgan yili: ")
-    narhi = input("Narhi: ")
-    # Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida
-    # lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
-    avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
-    # Yana avto qo'shish-qo'shmaslikni so'raymiz
-    javob = input("Yana avto qo'shasizmi? (yes/no): ")
-    if javob == "no":
-        break
+# print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
+# avtolar = []  # salondagi avtolar uchun bo'sh ro'yxat
+# while True:
+#     print("\nQuyidagi ma'lumotlarni kiriting", end="")
+#     kompaniya = input("Ishlab chiqaruvchi: ")
+#     model = input("Modeli: ")
+#     rangi = input("Rangi: ")
+#     korobka = input("Korobka: ")
+#     yili = input("Ishlab chiqarilgan yili: ")
+#     narhi = input("Narhi: ")
+#     # Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida
+#     # lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
+#     avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
+#     # Yana avto qo'shish-qo'shmaslikni so'raymiz
+#     javob = input("Yana avto qo'shasizmi? (yes/no): ")
+#     if javob == "no":
+#         break
 
-print("\nSalonimizdagi avtolar:")
-for avto in avtolar:
-    if avto["narh"]:
-        narh = avto["narh"]
-    else:
-        narh = "Noma'lum"
-    print(
-        f"{avto['rang'].title()} {avto['model'].title()}, {avto['korobka']} korobka. Narhi: {narh}"
-    )
+# print("\nSalonimizdagi avtolar:")
+# for avto in avtolar:
+#     if avto["narh"]:
+#         narh = avto["narh"]
+#     else:
+#         narh = "Noma'lum"
+#     print(
+#         f"{avto['rang'].title()} {avto['model'].title()}, {avto['korobka']} korobka. Narhi: {narh}"
+#     )
