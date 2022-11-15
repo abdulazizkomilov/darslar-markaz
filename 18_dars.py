0a99e3b2f174b4b6591f5d8026bc8420bfc4d822
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-11-15 21:44:01 +0500

    Update 18_dars.py

--- 18_dars.py
+++ 18_dars.py
@@ -1,15 +1,15 @@
-# def talabani_bahola(ismlar):
-#     baholar = {}
-#     while ismlar:
-#         ism = ismlar.pop()
-#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
-#         baholar[ism] = int(baho)
-#     return baholar
+def talabani_bahola(ismlar):
+    baholar = {}
+    while ismlar:
+        ism = ismlar.pop()
+        baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+        baholar[ism] = int(baho)
+    return baholar
 
 
 
-# baho = talabani_bahola(["ali", "vali", "hasan", "husan"])
-# print(baho)
+baho = talabani_bahola(["ali", "vali", "hasan", "husan"])
+print(baho)
 
 
 
