df341898b5189269066424e1b898882c08ae9dc0
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-02-27 19:02:38 +0500

    Update 11_dars.py

--- 11_dars.py
+++ 11_dars.py
@@ -1,37 +1,47 @@
-# car = {"model": "ferrari", "rang": "qizil"}
+# car = {
+#        "model": "ferrari", 
+#        "rang": "qizil",
+#        "yil": 2020,
+# }
 # print(car["model"])
 # print(car["rang"])
+# print(car["yil"])
 
-# lugat = {"salom": "hello", "kitob": "book"}
+# lugat = {
+#     "salom": "hello",
+#     "kitob": "book",
+    
+# }
 # print(f"salomning inglizcha tarjimasi - {lugat['salom']}")
 
-# # Lug'atda istalgan ma'lumot turlarini saqlash mumkin
+# Lug'atda istalgan ma'lumot turlarini saqlash mumkin
 # talaba_0 = {  
 #     'ism':'murod olimov',
 #     'yosh': 22,
-#     't_yil': 2000
+#     't_yil': 2000,
 # }
 # print(f"""{talaba_0['ism'].title()}, 
 # {talaba_0['t_yil']}-yilda tu'gilgan,
-# {talaba_0['yosh']} yoshda""")
+# {talaba_0['yosh']} yoshda
+# """)
 
-# # # Yangi kalit so'z va qiymat qo'shish
+# # Yangi kalit so'z va qiymat qo'shish
 # talaba_0['kurs'] = 4
 # talaba_0['fakultet'] = 'informatika'
 # talaba_0['ism'] = 'abdulloh'
-# # print(talaba_0)
+# print(talaba_0)
 # print(f"{talaba_0['ism'].title()}, \
 #   {talaba_0['t_yil']}-yilda tu'gilgan,\
 #   {talaba_0['yosh']} yoshda, \
 #   {talaba_0['fakultet']} fakulteti,\
 #   {talaba_0['kurs']} kursda")
 
-# # Bo'sh lug'at
+# Bo'sh lug'at
 # talaba_1 = {}
 # talaba_1['ism'] = 'qobil rasulov'
 # talaba_1['kurs'] = 3
 # talaba_1['yosh'] = 20
-# print(talaba_1)
+# print(talaba_1, "\n")
 # print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
 
 # # Qiymatni yangilash
@@ -49,7 +59,7 @@
 # del talaba_0['t_yil']
 # print(talaba_0)
 
-# # Lu'gatlarni bir nechta qatorda yozish
+# Lu'gatlarni bir nechta qatorda yozish
 # telefonlar = {
 #     'ali':'iphone',
 #     'vali':'galaxy s9',
@@ -67,7 +77,7 @@
 # phone = telefonlar.get('ali','Bunday ism mavjud emas')
 # print(phone)
 
-# phone = telefonlar.get('hasan', "hasan dick yuq")
+# phone = telefonlar.get('hasan', "hasan dict yuq")
 # print(phone)
 
 # sonlar = {}
@@ -77,13 +87,12 @@
 # sonlar["b"] = b_1
 # print(sonlar)
 
+# sonlar = {}
+# key = input("keyni kiriting>>>  ").lower()
+# value = float(input(f"{key}ga valueni kiriting>>>  "))
+# sonlar[key] = value
 
-sonlar = {}
-key = input("keyni kiriting>>>  ").lower()
-value = input(f"{key}ga valueni kiriting>>>  ").lower()
-sonlar[key] = value
-
-print(sonlar)
+# print(sonlar)
 
 
 
