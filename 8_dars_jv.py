7ea72b74018b92355734651480d35bbab7a743c5
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-02 14:55:47 +0500

    Update 8_dars_jv.py

--- 8_dars_jv.py
+++ 8_dars_jv.py
@@ -1,19 +1,70 @@
 # #Yangi cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia'] degan ro'yxat tuzing, ro'yxat elementlarining birinchi harfini katta qilib konsolga chqaring. GM uchun ikkala harfni katta qiling.
 # cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
 # for car in cars:
-#   if car=='gm':
+#   if car == 'gm':
 #     print(car.upper())
 #   else:
 #     print(car.title())
     
 
-# #Yuqoridagi mashqni teng emas (!=) operatori yordamida bajaring.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Yuqoridagi mashqni teng emas (!=) operatori yordamida bajaring.
 # cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
 # for car in cars:
-#   if car!='gm':
+#   if car != 'gm':
 #     print(car.title())
 #   else:
 #     print(car.upper()) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 # # #Foydalanuvchi login ismini so'rang. Agar login admin bo'lsa, "Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?" xabarini konsolga chiqaring. Aks xolda, "Xush kelibsiz, {foydalanuvchi_ismi}!" matnini konsolga chiqaring.
@@ -23,10 +74,64 @@
 # else:
 #   print(f"Xush kelibsiz {login.title()}!")
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # #Foydalanuvchidan 2 ta son kiritishni so'rang. Agar ikki son bir-biriga teng bo'lsa, "Sonlar teng" ekan degan yozuvni konsolga chiqaring.
 # x = float(input("Birinchi sonni kiriting: "))
 # y = float(input("Ikkinchi sonni kiriting:"))
 # if x==y: print(f"Sonlar teng: {x}={y}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # #Foydalanuvchidan istalgan son kiritishni so'rang. Agar son manfiy bo'lsa konsolga "Manfiy son", agar musbat bo'lsa "Musbat son" degan xabarni chiqaring.
 # son = float(input("Istalgan son kiriting: "))
@@ -35,6 +140,25 @@
     
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # import math
