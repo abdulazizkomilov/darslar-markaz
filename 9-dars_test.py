8d237715c352844fa64563eabc3238b1c2af6bb7
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-17 13:57:43 +0500

    Create 9-dars_test.py

---
+++ 9-dars_test.py
@@ -0,0 +1,8 @@
+foy = ['12345', '54321']
+
+for i in range(1,6):
+    login = (str(input(f"Iltimos {i} - login kiriting: "))).lower()
+    if login in foy:
+        print("sajdfhksajdfhasjdfn")
+    
+print(foy)
