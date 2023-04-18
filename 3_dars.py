a94b07c110ae497aacbb6ffa71a92e0dd43c502a
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-18 10:41:07 +0500

    Update 3_dars.py

--- 3_dars.py
+++ 3_dars.py
@@ -19,16 +19,16 @@ import math
 a = 9
 b = 49
 
-print(math.sqrt(b)) #ildizdan chiqaradi
+# print(math.sqrt(b)) #ildizdan chiqaradi
 
-print(math.ceil(45.6)) #katta yaqiniga
+# print(math.ceil(45.6)) #katta yaqiniga
 
-print(math.floor(45.67)) #kichik yaqiniga
+# print(math.floor(45.67)) #kichik yaqiniga
 
-print(math.sin(90))  #sonning sin si
+# print(math.sin(90))  #sonning sin si
 
-print(math.factorial(5))  #1*2*3*4*5
+# print(math.factorial(5))  #1*2*3*4*5
 
-print(math.log(125, 5))  #(5*5*5) == 5**3
+# print(math.log(125, 5))  #(5*5*5) == 5**3
 
 print(math.cos(180))
