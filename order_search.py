de45d9077e87173c2cbcfdc96c02317ea4d6470b
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-29 15:54:05 +0500

    Create order_search.py

---
+++ order_search.py
@@ -0,0 +1,24 @@
+array = [4, 6, 1, 9]
+
+def func(arr):
+    for i in range(0, len(arr)):
+        for _ in range(i+1, len(arr)):
+            if arr[i] >= arr[_]:
+                arr[i], arr[_] = arr[_], arr[i]
+    return arr
+
+# print(func(array))
+
+
+
+
+list_1 = [1,2,3,4,5,6,8,9,10,12]
+
+def find(nums: list[int]) -> int:
+    N = len(nums)+1
+    sumE = N*(N+1)/2
+    sumA = sum(nums)
+    
+    return sumE-sumA
+
+print(find(list_1))
