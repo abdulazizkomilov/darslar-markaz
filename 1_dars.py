745890d568ebc5de3157f230d1b7c0611f5f99fb
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-09-11 15:49:53 +0500

    adds

--- 1_dars.py
+++ 1_dars.py
@@ -1,4 +1,7 @@
-# print("Assalomu alaykum")
+# print('Assalomu alaykum')
+# print(56)
+# print('56')
+
 
 # print('Hello world!')
 
@@ -9,19 +12,19 @@
 # # str() - string  {matnlar}
 # # int() - integir  {sonlar}
 
-# ism = input("Ismingizni kiriting: ")
+# ism = input("Ismingizni kiriting>  ")
 # print("Xush kelibsiz -", ism)
 
 # a = int(input("A sonni kiriting: "))
 # b = int(input("b sonni kiriting: "))
-# print("a + b =", a + b)
+# print(a, '+', b, '=', a + b)
 
-# ism = input("Assalomu alaykum foydalanuvchi isminzgizni kiriting: ")
-# print('Assalomu alaykum ', ism, 'dasurimizga xush kelibsiz!')
+# name = input("Assalomu alaykum foydalanuvchi isminzgizni kiriting: ")
+# print('Assalomu alaykum ', name, 'dasurimizga xush kelibsiz!')
 
 # son1 = 10
 # son2 = 33
-# print("sizning raqamingiz - ",son1)
+# print("son1 = ",son1)
 # print("son2 = ",son2)
 
 # a = 5
@@ -32,17 +35,72 @@
 # print("b =",b)
 # print("a =",a)
 
-# name = "Abdulaziz Komilov"
+# name = "Ali Olimov"
 # age = 20
-# oy = 5
 
 
 # print(name, "yoshim", age, "da", )
 
-# name = input("Ismingizni kiriting>>>  ")
-# age = int(input("Yoshingni kiriting>>>  "))
-# print("Assalomu alaykum ",name,"sizning yoshingiz ",age,"da")
 
+
+# ism = input("Ismingizni kiriting>>>  ")
+# yosh = input("Yoshingni kiriting>>>  ")
+# print("Assalomu alaykum ",ism,"sizning yoshingiz ",2023-yosh,"da")
+
+
+# a = 'olim hakimov'
+# b = 'KLAJDsdfsadfJDFN'
+# c = 'lkasdfl;sakjdf'
+# print(a)
+# print(a.capitalize())
+# print(a.title())
+# print(b.upper())
+# print(b.lower())
+
+# name = input("Ismingizni kiriting: ").lower()
+
+# print("Salom", name.title())
+
+
+# age = int(input("Yoshingizni kiriting: "))
+# print(2023-age)
+
+# # + - / *  
+# a = 21
+# b = 6
+# natija = a + b
+# print(natija)
+
+# son4 = a - b
+# print(a, "-", b, "=", son4)
+
+# a = 10
+# b = 4
+# natija = a * b
+# print(natija)
+
+# a = int(input("a sonni kiriting:  "))
+# b = int(input("b sonni kiriting:  "))
+# print(a, ":", b, "=", a / b)
+
+# % // **, <, >,
+# son1 = 10
+# son2 = 3
+# print(son1 / son2)
+# print(son1 % son2)
+# print(son1 // son2)
+# print(son2 ** 2)
+
+# a = 22
+# b = 54
+# print(a > b)
+# print(a < b)
+
+# # # ==, !=
+# son = 10 == 6
+# son2 = 27 != 26
+# print(son)
+# print(son2)
 
 
 
--- 2_dars.py
+++ 2_dars.py
@@ -1,73 +1,143 @@
-# # int, str
-# # float()
-# # boolen
-# # input, + - / * % // **, <, >, ==, !=
-# # .capitalize() so`z boshidagini katta
-# # .title() so`zlar bosh harflarini katta
-# # .lower() kichik
-# # .upper() katta
-
-# a = 'abdulaziz komilov'
-# b = 'KLAJDsdfsadfJDFN'
-# c = 'lkasdfl;sakjdf'
-# print(a)
-# print(a.capitalize())
-# print(a.title())
-# print(c.upper())
-# print(b.lower())
-
-# name = input("Ismingizni kiriting: ").lower()
-
-# print("Salom", name.title())
+# PI
+# Constanta
+# type()
+# PI = 3.14
+# AS = 55.90
+# a = 3
 
 
-# age = int(input("Yoshingizni kiriting: "))
-# print(2023-age)
-
-# # + - / *  
-# son1 = 21
-# son2 = 6
-# natija = son1 + son2
-# # print(natija)
-
-# son4 = son1 - son2
-# print(son1, "-", son2, "=", son4)
+# str() - string  {matnlar}
+# int() - integir  {sonlar}
+# float() - float {suzuvchi yani kasir sonlar (32.4, 8,1)}
+# bool() - boolen {True, False}
 
 # a = 10
-# b = 4
-# natija = a * b
-# print(natija)
-# print(type(natija))
-# f = 'lsa;kdflsa'
 
-# a = int(input("a sonni kiriting:  "))
-# b = int(input("b sonni kiriting:  "))
-# print(a, ":", b, "=", a / b)
+# PI = 3.14 
 
-# # % // **, <, >,
-# son1 = 10
-# son2 = 3
-# print(son1 / son2)
-# print(son1 % son2)
-# print(son1 // son2)
-# print(son1 ** 2)
-
-# a = 22
-# b = 54
-# print(a > b)
-# print(a < b)
+# DM = 77
 
 
+# import math
+
+# a = math.pi
+# print(a)
+
+# a = 9
+# b = 49
+
+# print(math.sqrt(b)) #ildizdan chiqaradi
+
+# print(math.ceil(45.6)) #katta yaqiniga 46
+
+# print(math.floor(45.67)) #kichik yaqiniga 45
+
+# print(math.sin(90))  #sonning sin si
+
+# print(math.factorial(5))  #1*2*3*4*5
+
+# print(math.log(125, 5))  #(5*5*5) == 5**3
+
+# print(math.cos(180))
 
 
 
-# # ==, !=
-# son = 10 == 6
-# son2 = 27 != 26
-# son3 = 27 != 27
-# print(son)
-# print(son2)
-# print(son3)
+
+# list -- ro'yxat
+# #                 0      1      2           3            4           5 
+# qurulmalar = ["telfon", 'TV', 'wifi', 'sichqoncha', 'headphones', 'lamp']
+# print(qurulmalar[-1].lower())
+
+# # .upper()
+# # .lower()
+
+# # # ismlar degan ro'yxat yarating va kamida 3 ta yaqin do'stingizning ismini kiriting
+# ismlar = ["ali", "olim", "vali"] 
+
+# # # Ro'yxatdagi har bir do'stingizga qisqa xabar yozib konsolga chiqaring:
+# # print("Salom " + ismlar[0] + ismlar[1] + ismlar[-1] + " ishlaring yaxshimi?")
+# print(f"Salom {ismlar[0].upper()} {ismlar[1].lower()}, {ismlar[-1]} ishlaring yaxshimi?")
+
+# yil = input("Tugùlgan yilizni kiriting:  ")
+# print(f'Siz {yil} - yilda tug`ulgansiz.')
+
+
+# f_name = input("ism familya:  ")
+# year = int(input("tug yil kiriting:  "))
+# address = input("manzil:  ")
+# phone = int(input("tel:  "))
+# print(f"""Salom {f_name.title()}, sizning yoshingiz {2022-year}da siz, 
+# manzil: {address.capitalize()},
+# tel: {phone}""")
+
+
+# print(f"  ")   # f - string
+# sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy, butun, o'nlik).
+
+sonlar = [22, -58.2, 34.0, 67, 1983, 123_456_000_0, 112.4]
+print(sonlar)
+print(sonlar[-1])
+
+# Yuqoridagi ro'yxatdagi sonlar ustida turli arifmetik amallar bajarib ko'ring. Ro'yxatdagi ba'zi sonlarning qiymatini o'zgartiring, ba'zilarini esa almashtiring.
+sonlar[0] = sonlar[0] + sonlar[-1]
+sonlar[1] = -67.8
+sonlar[4] = sonlar[4] + 40
+sonlar[3] = 77
+print(sonlar)
+del sonlar[5]
+print(sonlar)
+del sonlar[0]
+print(sonlar)
+
+t_shaxslar = ["amir temur", "Imom Buxoriy", "Napoleon"]
+z_shaxslar = ["Bill Gates", "Elon Musk", "Doasnald Trump"]
+
+# # # Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
+print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(0).title()} \n\
+bilan, zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
+suhbat qilishni istar edim\n")
+
+print(f"""Men tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,
+zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan
+suhbat qilishni istar edim""")
+
+
+# friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga 
+# chaqirmoqchi bo'lgan do'stlaringizni kiriting.
+# friends = []
+# friends.append("John")
+# friends.append("Alex")
+# friends.append("Danny")
+# friends.append("Sobirjon")
+# friends.append("Vanya")
+# print(f"To`liq list-{friends}")
+
+# # # # Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang.
+# friends.remove("John")
+# friends.remove("Alex")
+# friends.remove("Vanya")
+# print(f"Kelganlar-{friends}")
+
+
+# friends = []
+# # # Ro'yxatning oxiriga, boshiga va o'rtasiga yangi ismlar qo'shing.
+# friends.append("Hasan")
+# friends.insert(0, "Husan")
+# friends.insert(2, "Ivan")
+# friends.insert(3, "ali")
+# print(friends)
+
+# # Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. 
+# # .pop() va .append() metodlari yordamida mehmonga kelgan 
+# # do'stlaringizning ismini friends ro'yxatidan sug'urib olib, mehmonlar ro'yxatiga qo'shing.
+# mehmonlar = []
+# mehmonlar.append(friends.pop(1))
+# mehmonlar.append(friends.pop(-1))
+# mehmonlar.append(friends.pop(0))
+# mehmonlar.insert(2, friends.pop(0))
+# print("\nKelgan mehmonlar: ", mehmonlar)
+
+
 
 
 
