c118b208cfcf195c97fb20623d87311d3ecc72fe
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-10-20 14:06:52 +0500

    Create 12_1_dars_jv.py

---
+++ 12_1_dars_jv.py
@@ -0,0 +1,15 @@
+# def multiply(*sonlar):
+#     kopaytma = 1
+#     for son in sonlar:
+#         kopaytma *= son
+#     return kopaytma
+
+# print(multiply(4,5,6))
+
+
+# def talaba_info(ism, familiya, **kwargs):
+#     kwargs['ism']=ism
+#     kwargs['familiya']=familiya
+#     return kwargs
+
+# talaba = talaba_info('olim','olimov',tyil=1995,fakultet='IT',yonalish='AT')
