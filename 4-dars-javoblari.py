4a1fd01eaac783ced4a84401d943dcf629d869d3
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-10-20 14:08:10 +0500

    Create 4-dars-javoblari.py

---
+++ 4-dars-javoblari.py
@@ -0,0 +1,35 @@
+# ismlar = ['a', 'v', 'f']
+
+# n = 0
+# for i in ismlar:
+#     n = n + 1
+#     print(f"{n},  {i}")
+    
+# print(f"{n} martta")  
+    
+# kinolar = []
+
+# for _ in range(5):
+#     f1 = input(f"{_+1}-kino nomini kiriting:  ")
+#     kinolar.append(f1)
+    
+# print(kinolar)
+
+
+# ismlar = []
+
+# for _ in range(int(input("Nechta odam bn ko'rishdingiz:  "))):
+#     f1 = input(f"{_+1}-kino nomini kiriting:  ")
+#     ismlar.append(f1)
+    
+# print(ismlar)
+
+
+
+
+
+
+
+
+
+
