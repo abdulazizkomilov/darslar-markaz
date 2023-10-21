995ed2502c2f478cf99272cc225a2f718f525b3d
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-10-21 14:18:05 +0500

    Create amaliyot.py

---
+++ amaliyot.py
@@ -0,0 +1,22 @@
+# 3. Python izohli lu'gati tuzing: Lug'atga shu kunga qadar 
+# o'rgangan 10 ta so'z (atamani) kiriting (masalan integer, 
+# float, string, if, else va hokazo) va har birining 
+# qisqacha tarjimasini yozing.
+
+# 4. Foydalanuvchidan biror so'z kiritishni so'rang 
+# va so'zning tarjimasini yuqoridagi lug'atdan chiqarib 
+# bering. Agar so'z lu'gatda mavjud bo'lmasa, 
+# "Bunda so'z mavjud emas" degan xabarni chiqaring.
+
+# Consolda:
+#         Kalit suz kiriting: integer 
+#         Bu son
+
+# 5. Yuqoridagi vazifani if-else yordamida qiling va 
+# natijani ham foydalanuvchiga tushunarli ko'rinishda chiqaring.
+
+# Consolda: 
+#         Kalit so'z kiriting:float
+#         Float so'zi O'nlik son deb tarjima qilinadi
+#         Kalit so'z kiriting:dictionary
+#         Bunday so'z mavjud emas
