8ff32949f123c263a0e708f0d26077aabed2d657
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-10-23 17:32:35 +0500

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,68 @@
+
+
+
+# python_dict = {
+#      "int": "sonlar",
+#      "string": "matinlar"
+#  }
+
+# for key, value in python_dict.items():
+#     print(f"{key.capitalize()} - {value.capitalize()}")
+
+
+# county = {
+#     "aqsh": "washington d.c.", 
+#     "uzb": "tashkent", 
+#     "singapur": "singapur"
+#     }
+
+# print("Dunyo davlatlari: ")
+# for davlat in sorted(county):
+#     print(davlat.upper())
+
+# print("\nDavlatlar poytaxtlari: ")
+# for poytaxt in sorted(county.values()):
+#     print(poytaxt.title())
+
+
+# county = {
+#     "aqsh": "washington d.c.", 
+#     "uzb": "tashkent", 
+#     "singapur": "singapur"
+#     }
+
+# capital = input("Qaysi davlat poytaxtini bilishni istaysiz: ")
+# capital = county.get(capital)
+# if capital == None:
+#     print("Kechirasiz, Bunday ma`lumot bizda yuq!")
+# else:
+#     print(capital)
+
+# taomlar = {}
+# for i in range(5):
+#     key = input(f"{i+1}-taomni kiriting: ")
+#     value = int(input(f"{key} narxini kiriting: "))
+#     taomlar[key] = value
+
+# buyurtmalar = []
+# for n in range(3):
+#     buyurtmalar.append(input(f"{n+1}-taomga nima buyurtma qilasiz?  "))
+
+# for buyurtma in buyurtmalar:
+#     if buyurtma in taomlar:
+#         print(f"{buyurtma} - {taomlar[buyurtma]}")
+#     else:
+#         print(f"{buyurtma} yuq")
+    
+
+
+
+
+
+
+
+
+
+
+
+
