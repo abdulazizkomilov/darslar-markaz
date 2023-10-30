d958b4453d376a71d02f237ac416a254d593295e
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-10-30 14:03:30 +0500

    Update 1_dars.py

--- 1_dars.py
+++ 1_dars.py
@@ -1,41 +1,53 @@
 # print('Assalomu alaykum')
-# print("Salom")
-# print(56+6)
+# print("HI")
+# print(56 + 6)
 # print(56)
 # a = 'olim'
-
 # print(a)
-
+# print('77 + 2')
 # print('Hello world!')
+# b = 5
+# c = 16
+# d = 9
+# yigindi = b + c
+# bulinma = yigindi / d
+# print(yigindi)
+# print(bulinma)
 
 
-# # str() - string  {matnlar}
-# # int() - integir  {sonlar}
+# str() - string  {matnlar}
+# int() - integir  {sonlar}
 
-# ism = input("Ismingizni kiriting>  ")
+# ism = input("Ismingizni kiriting:  ")
 # print("Xush kelibsiz -", ism)
 
 
-# a = int(input("A sonni kiriting: "))
-# b = int(input("b sonni kiriting: "))
+
+# a = int(input("a kirit "))
+# b = int(input("kiyingi sonni kiriting "))
 # print(a, '+', b, '=', a + b)
 
 
+
+
 # name = input("Assalomu alaykum foydalanuvchi isminzgizni kiriting: ")
-# print('Assalomu alaykum ', name, 'dasurimizga xush kelibsiz!')
+# print('Assalomu alaykum', name, 'dasurimizga xush kelibsiz!')
+
+
 
 # son1 = 10
 # son2 = 33
-# print("a = ",son1)
-# print("b = ",son2)
+# print("a = ", son1)
+# print("son2 ning qiymati", son2)
+ 
 
 # a = 5
 # b = 6
 # c = 'assalomu alaykum'
 
-# print("c =",c)
-# print("b =",b)
-# print("a =",a)
+# print("c =", c)
+# print("b =", b)
+# print("a =", a)
 
 # name = "Ali Olimov"
 # age = 20
@@ -47,17 +59,28 @@
 
 # ism = input("Ismingizni kiriting>>>  ")
 # yosh = int(input("Yoshingni kiriting>>>  "))
-# print("Assalomu alaykum ",ism,"sizning tug'ulgan yilingiz ",2023-yosh,"-yil da")
+# print(ism, "sizning tug'ulgan yilingiz ", 2023 - yosh, "-yil da")
 
 
 # a = "olim hakimov jamol o'g'li"
+
 # b = 'KLAJDsdfsadfJDFN'
+
 # c = 'lkasdflsaLJSADFkjdf'
+
 # print(a)
+
 # print(a.capitalize())
+
 # print(a.title())
+
 # print(b.upper())
+
 # print(b.lower())
+
+
+
+
 
 # name = input("Ismingizni kiriting: ").lower()
 
@@ -67,11 +90,13 @@
 # age = int(input("Yoshingizni kiriting: "))
 # print(2023-age)
 
-# # + - / *  
+
+
+# + - / *
 # a = 21
 # b = 6
 # natija = a + b
-# print(natija)
+# print(a, '+', b, '=', natija)
 
 # son4 = a - b
 # print(a, "-", b, "=", son4)
@@ -81,30 +106,43 @@
 # natija = a * b
 # print(natija)
 
-# a = int(input("a sonni kiriting:  "))
+# a = int(input('a sonni kiriting:  '))
 # b = int(input("b sonni kiriting:  "))
 # print(a, ":", b, "=", a / b)
 
-# # % // **, <, >,
-# son1 = 10
-# son2 = 3              # 3,    1 q
+
+# % // **, <, >,
+son1 = 10
+son2 = 3              # 3,    1 q
+
 # print(son1 / son2)    # 3.3333
 # print(son1 % son2)    # 1  
 # print(son1 // son2)   # 3
-# print(son2 ** 3)      # 9
+# print(son2 ** 2)      # 9
+
+# print(son1, ':', son2, '=',son1//son2,'butun qismi, ', son1%son2, 'qoldiq qismi.')
+
+
+# f" ..., {son1+son2}  ...? {son2} "
+
+# son1 = 10
+# son2 = 3 
+
+# butun_q = son1 // son2
+# qoldiq_q = son1 % son2
+# print(f"{son1} : {son2} = {butun_q} butun qismi, {qoldiq_q}, qoldiq qismi.")
+
 
 # a = 22
 # b = 54
-# print(a > b) #xato
-# print(a < b) #tugri
-
+# print(a > b) #xato -- False
+# print(a < b) #tugri - True
 
 # # str() - string  {matnlar}
 # # int() - integir  {sonlar}
-# # float() - kasr sonlar
+# # float() - kasr sonlar  7.6, 6.09
 # # bool() - True, False
  
-
 # # # ==, !=
 # son = 10 == 6 # f
 # son2 = 27 != 26 # t
