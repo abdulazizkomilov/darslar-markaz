57c495b7626dcf5aed7b2cef906aad1ee9dea08c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-11-08 21:45:55 +0500

    Update random.py

--- random.py
+++ random.py
@@ -1,13 +1,14 @@
-# import random as r
+import random as r
 
 # # randint()
 # son = r.randint(0,50)
 # print(son)
 
-# # choice()
+# # # choice()
 # ismlar = ['olim','anvar','hasan','husan']
 # ism = r.choice(ismlar)
 # print(ism)
+
 # print(r.choice(ism))
 # x = list(range(0,51,5))
 # print(x)
@@ -20,15 +21,54 @@
 # print(x)
 
 
-# ismlar = ['marjona', 'o`g`iloy', 'shohjaxon', 
-#           'ahad', 'javohir', 'doston', 'kamron', 
-#           'anvar', 'davlatbek', 'ramazon',  
-#           'bobur', 'xurshidbek']
+# ismlar = ['zarnigor', 'muslima', 'samandar', 'og`abek', 'ramazon']
+
 # print(f'ismlar uzunligi {len(ismlar)}')
 # print(ismlar)
+
 # r.shuffle(ismlar)
 # print(ismlar)
 # print(r.choice(ismlar))
+
+# son1 = r.randint(1,50)
+# son2 = r.randint(11, 21)
+# print(f"Javobni toping: \n{son1} + {son2} = ?")
+# javob = int(input(">>>  "))
+# if javob == son1+son2:
+#     print('Tugri')
+# else:
+#     print('Xato')
+
+
+amallar = {
+    1:'+', 
+    2:'-',
+    3:'x',
+}
+son1 = r.randint(1,10)
+son2 = r.randint(1, 10)
+son = r.randint(1,3)
+amal = amallar[son]
+print(f"Javobni toping: \n{son1} {amal} {son2} = ?")
+javob = float(input(">>>  "))
+if amal == '+':
+    if javob == son1+son2:
+        print('Tugri')
+    else:
+        print('Xato')
+if amal == '-':
+    if javob == son1-son2:
+        print('Tugri')
+    else:
+        print('Xato')
+if amal == 'x':
+    if javob == son1*son2:
+        print('Tugri')
+    else:
+        print('Xato')
+
+
+
 
 
 
