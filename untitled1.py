208f9717add55d87b1ca4b3c3f30f44e481c836f
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-12-06 11:16:13 +0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,141 +1,50 @@
-class Shaxs:
-    """Shaxslar haqida ma'lumot"""
+from datetime import datetime, timedelta
 
-    def __init__(self, ism, familiya, passport, tyil):
-        """Shaxsning xususiyatlari"""
-        self.ism = ism
-        self.familiya = familiya
-        self.passport = passport
-        self.tyil = tyil
+class UserLimit:
+    def __init__(self):
+        self.usage_count = 0
+        self.last_reset_date = datetime.now()
 
-    def get_info(self):
-        """Shaxs haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya}. "
-        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
-        return info
+    def reset_daily_limit(self):
+        if datetime.now().date() > self.last_reset_date.date():
+            self.usage_count = 0
+            self.last_reset_date = datetime.now()
+            
+    def get_current_usage(self):
+        return self.usage_count
 
-    def get_age(self, yil):
-        """Shaxsning yoshini qaytaruvchi metod"""
-        return yil - self.tyil
-    
-    
-class Talaba(Shaxs):
-    """Talaba klassi"""
+    def increment_usage(self):
+        self.usage_count += 1
 
-    def __init__(self, ism, familiya, passport, tyil, idraqam, manzil):
-        """Talabaning xususiyatlari"""
-        super().__init__(ism, familiya, passport, tyil)
-        self.idraqam = idraqam
-        self.bosqich = 1
-        self.manzil = manzil
-        
-    def __repr__(self):
-        info = f"{self.ism} {self.familiya} "
-        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
-        return info
-
-    def get_id(self):
-        """Talabaning ID raqami"""
-        return self.idraqam
-
-    def get_bosqich(self):
-        """Talabaning o'qish bosqichi"""
-        return self.bosqich
-
-    def get_info(self):
-        """Talaba haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya} "
-        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
-        return info
-
-shaxs1 = Shaxs("olim", "karimov", "AB3457261", 1997)
-shaxs2 = Shaxs("vali", "kamolov", "BC7922639", 1980)
-
-talaba = Talaba("olim", "karimov", "AB3457261", 1997, "ID56782", "yangiyer")
-talaba1 = Talaba("Valijon", "Aliyev", "FA112299", 2000, "0000012", "samarqand")
-
-class Manzil:
-    """Manzil saqlash uchun klass"""
-
-    def __init__(self, uy, kocha, tuman, viloyat):
-        """Manzil xususiyatlari"""
-        self.uy = uy
-        self.kocha = kocha
-        self.tuman = tuman
-        self.viloyat = viloyat
-        
-    def __repr__(self):
-        manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
-        manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
-        return manzil
-
-    def get_manzil(self):
-        """Manzilni ko'rish"""
-        manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
-        manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
-        return manzil
-
-talaba1_manzil = Manzil(12, "Olmazor", "Bog'bon", "Samarqand")
-talaba2 = Talaba("Valijon", "Aliyev", "FA112299", 2000, "0000012", talaba1_manzil)
+    def has_daily_limit_exceeded(self, limit):
+        self.reset_daily_limit()
+        return self.usage_count > limit
 
 
+user_limits = {}
+
+daily_limit = 5
+
+def handle_user(user_id):
+    user_limit = user_limits.get(user_id, UserLimit())
+    user_limits[user_id] = user_limit
+
+    if user_limit.has_daily_limit_exceeded(daily_limit):
+        return "Sizning kunlik limitlariniz tugadi. Yangi limit ertaga belgilandi."
+        user_limit.reset_daily_limit()
+    else:
+        user_limit.increment_usage()
 
 
-class University:
-    def __init__(self, name):
-        """Manzil xususiyatlari"""
-        self.name = name
-        self.students_list = []
-        self.students_nums = 0
-        
-    def __repr__(self):
-        return f"{self.name} universiteti"
+def get_user_usage(user_id):
+    user_limit = user_limits.get(user_id, UserLimit())
+    user_limits[user_id] = user_limit
 
-    def __len__(self):
-        return len(self.students_list)
+    if user_limit.has_daily_limit_exceeded(daily_limit):
+        user_limit.reset_daily_limit()
 
-    def __getitem__(self, index):
-        return self.students_list[index]
-
-    def __setitem__(self, index, value):
-        if isinstance(value, Talaba):
-            self.students_list[index] = value
-
-    def __add__(self, qiymat):     # salon3 = salon1 + salon2 / salon1 + avto6
-        if isinstance(qiymat, University):
-            yangi_uni = University(f"{self.name} {qiymat.name}")
-            yangi_uni.students_list = self.students_list + qiymat.students_list
-            return yangi_uni
-        elif isinstance(qiymat, Talaba):
-            self.add_student(qiymat)
-        else:
-            print(f"Universitetga {type(qiymat)} qo`shib bo`lmaydi")
-
-    def __call__(self, *param):
-        if param:
-            for student in param:
-                self.add_student(student)
-        else:
-            return [student for student in self.students_list]
-
-    def add_student(self, *qiymat):
-        for student in qiymat:
-            if isinstance(student, Talaba):
-                self.students_list.append(student)
-            else:
-                print("Talaba obyketini kiriting")
-
-    def get_list(self):
-        return [student for student in self.students_list]
+    return user_limit.get_current_usage()
 
 
-
-
-uni1 = University("TATU")
-uni2 = University("SAMDU")
-uni3 = University("INHA")
-
-
-uni1.add_student(talaba, talaba1)
-uni2(talaba2)
-
+test_user_id = 123456789
+handle_user(test_user_id)
