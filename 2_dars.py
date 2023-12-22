8713f56e2a7a4b96dd9fd918b30c77632385b31c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-12-22 17:24:46 +0500

    Update 2_dars.py

--- 2_dars.py
+++ 2_dars.py
@@ -1,8 +1,7 @@
-# Constanta
-# type()
+# # Constanta
+# # type()
 # PI = 3.14
 # G = 9.81
-# AS = 55.90
 # A = 3
 # OYLIK = 300_000
 
@@ -22,7 +21,7 @@
 
 # print(math.sin(90))  #sonning sin si
 
-# print(math.factorial(5))  # 1*2*3*4
+# print(math.factorial(1000))  # 1*2*3*4*5
 
 # print(math.log(144, 5))  #(5*5*5) == 5**3
 
@@ -32,20 +31,18 @@
 
 # list -- ro'yxat
 #  index         0       1      2           3         4|-2          5|-1
-
 # qurulmalar = ["telfon", 'TV', 'wifi', 'sichqoncha', 'headphones', 'lamp']
-# print(qurulmalar[1].lower())
-
+# print(qurulmalar[1])
+# print(qurulmalar[-1])
 
 
 # # ismlar degan ro'yxat yarating va kamida 3 ta yaqin do'stingizning ismini kiriting
 # ismlar = ["ali", "olim", "vali"]
 
-# Ro'yxatdagi har bir do'stingizga qisqa xabar yozib konsolga chiqaring:
-# print("Salom " + ismlar[0] + ismlar[1] + ismlar[-1] + " ishlaring yaxshimi?")
-# print(f"Salom {ismlar[0].upper()}, \
-# {ismlar[1].lower()}, {ismlar[-1]} \
-# ishlaring yaxshimi?")
+# # # Ro'yxatdagi har bir do'stingizga qisqa xabar yozib konsolga chiqaring:
+# print(f"""Salom {ismlar[0].upper()}, 
+# {ismlar[1].title()}, {ismlar[-1].title()} 
+# ishlaring yaxshimi?""")
 
 
 # f_name = input("ism familya kiriting:  ")
@@ -59,19 +56,19 @@
 
 
 # print(f"  ")   # f - string
+
 # sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy, butun, o'nlik).
 
-# sonlar = [22, -58.2, 34.0, 67, 1983, 123_456_000_1, 112.4]
+# sonlar = [22, -58.2, 34.0, 67, 1983, 123_450_001, 112.4]
 # print(sonlar)
 # print(sonlar[-1])
-
 
 # sonlar[0] = sonlar[0] + sonlar[-1]
 # sonlar[1] = -67.8
 # sonlar[4] = sonlar[4] + 40
 # sonlar[3] = 77
 # print(sonlar)
-# del sonlar[5]
+# del sonlar[5]  # delete - o'chirish
 # del sonlar[0]
 # print(sonlar)
 
@@ -79,31 +76,29 @@
 # friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga 
 # # chaqirmoqchi bo'lgan do'stlaringizni kiriting.
 
-# friends = []
-# friends.append("John")
-# friends.append("Alex")
-# friends.append("Danny")
-# friends.append("Sobirjon")
-# friends.append("Vanya")
-# print(f"To`liq list: {friends}")
-
+friends = []
+friends.append("John")
+friends.append("Alex")
+friends.append("Danny")
+friends.append("Sobirjon")
+friends.append("Vanya")
+print(f"To`liq list: {friends}")
 
 # # # Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang.
-# friends.remove("John")
-# friends.remove("Alex")
-# del friends[-1]
-# print(f"Kelganlar-{friends}")
-
+friends.remove("John")
+friends.remove("Alex")
+del friends[-1]
+print(f"Kelganlar-{friends}")
 
 # talaba = ['jon', 'vaniya', 'kamol']
-# print(len(talaba))
+# print(talaba)
 # talaba.append("Hasan")
 # talaba.insert(0, "Husan")
 # talaba.insert(2, "Ivan")
 
 # print(talaba)
-
 # print(len(talaba))
+
 
 # Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. 
 # .pop() va .append() metodlari yordamida mehmonga kelgan 
