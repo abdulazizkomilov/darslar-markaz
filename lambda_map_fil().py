82f21d43bf3254307837cc8363b5fef82f4e4a69
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-04-11 11:00:16 +0500

    Update lambda_map_fil().py

--- lambda_map_fil().py
+++ lambda_map_fil().py
@@ -1,24 +1,26 @@
-import math
-
+# import math
 
 # def daraja(x):
 #     ifoda = x * x
 #     return ifoda
 
-
-# daraja_2 = lambda x, y: x ** y
-# print(daraja_2(2, 5))
-
+# daraja_2 = lambda x: x * x
+# print(daraja_2(5))
 
 # uzunlik = lambda pi, r: 2*pi*r
 # print(uzunlik(math.pi, 5))
+
+# sonlar_3 = lambda x, y, z: x * y * z
+
+# print(sonlar_3(3, 4, 5))
+
 
 
 
 # # def kbvadrat(x, y):
 
 # kvadrat = lambda x, y: x ** y
-# print(kvadrat(3, 2))
+# print(kvadrat(9, 8))
 
 
 # def daraja(n):
@@ -43,27 +45,29 @@ import math
 
 
 
-# from math import sqrt  # sqrt - kvadrat ildiz
+# from math import sqrt, factorial  # sqrt - kvadrat ildiz
 
 # sonlar = list(range(11))  # 0 dan 10 gacha sonlar ro'yxati
-# ildizlar = list(map(sqrt, sonlar)) 
+# ildizlar = list(map(factorial, sonlar)) 
+# print(ildizlar)
 
 
-# # # map(funksiya, qiymatlar) -- list(map()) 
-# # # qiymatlar ichidagi har bir son uchun 
-# # # ishlaydi
+
+# map(funksiya, qiymatlar) -- list(map()) 
+# qiymatlar ichidagi har bir son uchun 
+# ishlaydi
 
 # print(ildizlar)
 
 
-# names = ['ali', 'vali', 'olim', 'hakim', 'husan', 'hasan']
+names = ['ali', 'vali', 'olim', 'hakim', 'husan', 'hasan']
 
-# # def ism_upper(arr):
-# #     ismlar = []
-# #     for ism in names:
-# #         ismlar.append(ism.upper())
-# #     return ismlar
-# # print(ism_upper(names))
+# def ism_upper(arr):
+#     ismlar = []
+#     for ism in names:
+#         ismlar.append(ism.upper())
+#     return ismlar
+# print(ism_upper(names))
 
 # print(list(map(lambda ism: ism.upper(), names)))
 
@@ -83,12 +87,14 @@ import math
 
 
 
-# sonlar = list(range(11))
+sonlar = list(range(11))
 
 # def son_t(x):
 #     if x % 2 == 1:
 #         return x
 
+# print(list(map(son_t, sonlar)))
+      
 
 # print(list(map(son_t, sonlar)))
 
@@ -102,7 +108,9 @@ import math
 
 # a = [4, 5, 6]
 # b = [7, 8, 9]   
-# # z = [3, 7, 2]
+
+# # # c = [3, 7, 2]
+# # # a+b+c
 
 # a_plus_b = list(map(lambda x, y: x + y, a, b))
 # print(a_plus_b)
@@ -110,31 +118,38 @@ import math
 
 
 
-# import random as r
+import random as r
 
-# sonlar = r.sample(range(100), 10)  # 0-99 oralig'ida 10 ta tasodifiy sonlar
-# print(sonlar)
+sonlar = r.sample(range(100), 10)  # 0-99 oralig'ida 10 ta tasodifiy sonlar
+print(sonlar)
+
 
 # def juftmi(x):
 #     """x juft bo'lsa True, aks holda False qaytaruvchu funksiya"""
 #     return x % 2 == 0
 
-# juft_sonlar = list(filter(lambda x: x % 2 == 0, sonlar))
-# print(juft_sonlar)
+
+# print(list(filter(juftmi, sonlar)))
+
+
 
 
 # juft_sonlar = list(filter(lambda x: x % 2 == 1, sonlar))
 # print(juft_sonlar)
 
 
-# import random as r
+import random as r
+
+mevalar1 = ["olma", "limon", "sabzi", 
+            "kivi", "olcha", "olxuri", 
+            "sarimsoq", "salim"]
+
+# filtered_fruits = list(filter(lambda meva: meva.startswith('s'), mevalar1))
+# print(filtered_fruits)
 
 
-# mevalar1 = ["olma", "limon", "sabzi", 
-#             "kivi", "olcha", "olxuri", "sarimsoq", "sads"]
 
-# mevalar = list(filter(lambda meva: meva.startswith('o'), mevalar1))
-# print(mevalar)
+
 
 
 # mevalar2 = list(filter(lambda meva: len(meva) <= 5, mevalar1))
@@ -142,8 +157,6 @@ import math
 
 # print(list(filter(lambda meva: (meva.startswith("s") and meva.endswith("i")), mevalar1)))
 
-
-# print(list(filter(lambda meva: (meva.startswith("s") and len(meva) >= 5), mevalar1)))
 
 
 
@@ -196,14 +209,17 @@ import math
 # import random as r
 
 # son1 =r.randint(0, 10)
-# son2 =r.randint(5, 15)
-# son3 =r.randint(20, 30)
+# son2 =r.randint(0, 10)
+# son3 =r.randint(0, 10)
 # natija = int(input(f"Hisoblang:\n{son1} + {son2} + {son3} = "))
 # n = son1+son2+son3
 # if natija == n:
 #     print(f"To'g'ri. Javob: {n}")
 # else:
 #     print(f"Noto'g'ri. Javob: {n}")
+
+
+
 
 
 
