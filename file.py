3d41b1c736761dc5bb63cb7dc4b684590ac63863
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-04-13 15:09:38 +0500

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-# file = open("pi.txt")
+# file = open("pi-son.txt")
 # PI = file.read()
 # print(PI)
 # file.close()
@@ -9,13 +9,15 @@
 #     pi = file.read()   
 # print(pi)
 
+
 # pi = pi.rstrip()  #yangi qatorni olib tashlaydi
 # pi = pi.replace('\n', "")  #o`rniga almashtiradi
-# pi = float(pi)
+# pi = str(pi)
 # print(pi)
 
 
-filename = "data/talabalar.txt"
+
+# filename = "data/talabalar.txt"
 
 # with open(filename) as file:
 #     for line in file:
@@ -35,51 +37,56 @@ filename = "data/talabalar.txt"
 
 # # yozish  "W" write ustidan yozib yuboradi
 
-faylnomi = "new_file.txt"
+# faylnomi = "test_file.txt"
 # ism = "Umar Hasanov"
-# tyil = 1999
-# address = 'y'
+# t_yil = 1999
+# address = 'yangiyer'
 # with open(faylnomi, "w") as fayl:
 #     fayl.write(ism + '\n')
-#     fayl.write(str(tyil)+'\n')
-#     fayl.write(address+'\n')
+#     fayl.write(str(t_yil) + '\n')
+#     fayl.write(address + '\n')
 
 
+
+# faylnomi = "test_file.txt"
 # malumotlar = ["alivey vali", 2000, 9837297293]
+
 # # qo`shish yoki yangi yasash "a" append
+
 # with open(faylnomi, "a") as fayl:
-#     for _ in malumotlar:
-#         fayl.write(str(_)+'\n')
+#     for i in malumotlar:
+#         fayl.write(str(i)+'\n')
 
 
-import pickle
 
-talaba1 = {
-    "ism": "hasan", 
-    "familiya": "husanov", 
-    "tyil": 2003, 
-    "kurs": 2}
 
+
+# import pickle
+
+# talaba1 = {
+#     "ism": "hasan", 
+#     "familiya": "husanov", 
+#     "tyil": 2003, 
+#     "kurs": 2}
 
 # # # datalarni yozish wb - write binary  // ko`rib bo`lmaydi editorda
 # with open("info", "wb") as file:
 #     pickle.dump(talaba1, file)
     
-
 # # datani o`qish rb - read binary  // 2lik sanoq sistemada
 # with open("info", "rb") as file:
 #     talaba_1 = pickle.load(file)
 # print(talaba_1)
 
 
-while True:
-    a = input("ism k.  (0|1):  ")
-    if a != 0:
-        b = input("q k: ")
-        d = {}
-        d[a] = b
-        with open("aaa.txt", 'a') as file:
-            file.write(str(d)+'\n')
-    else:
-        break
+# while True:
+#     a = input("ism k.  (0|1):  ")
+#     if a != 0:
+#         b = input("q k: ")
+#         d = {}
+#         d[a] = b
+#         with open("aaa.txt", 'a') as file:
+#             file.write(str(d)+'\n')
+#     else:
+#         break
 
