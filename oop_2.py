91178b72919b6f1ec854252283bb472ef42168ac
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-04-30 14:24:38 +0500

    Update oop_2.py

--- oop_2.py
+++ oop_2.py
@@ -1,22 +1,22 @@
-class Talaba:
-    """Talaba nomli klass yaratamiz"""
-    def __init__(self, ism, familiya, tyil):
-        """Talabaning xususiyatlari"""
-        self.ism = ism
-        self.familiya = familiya
-        self.tyil = tyil
-        self.bosqich = 1
+# class Talaba:
+#     """Talaba nomli klass yaratamiz"""
+#     def __init__(self, ism, familiya, tyil):
+#         """Talabaning xususiyatlari"""
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.tyil = tyil
+#         self.bosqich = 1
     
-    def get_info(self):
-        return f"""{self.ism} {self.familiya} 
-{self.tyil} da tug`ulgan. 
-{self.bosqich}-bosqich talabasi"""
+#     def get_info(self):
+#         return f"""{self.ism} {self.familiya} 
+# {self.tyil} da tug`ulgan. 
+# {self.bosqich}-bosqich talabasi"""
     
-    def set_bosqich(self, qiymat):
-        self.bosqich = qiymat
+#     def set_bosqich(self, qiymat):
+#         self.bosqich = qiymat
         
-    def update_bosqich(self):
-        self.bosqich += 1
+#     def update_bosqich(self):
+#         self.bosqich += 1
 # talaba1 = Talaba("Alijon", "Valiyev", 2000)
 
 # talaba_2 = Talaba("Abror", "Muminboyev", 2010)
@@ -30,28 +30,28 @@ class Talaba:
 # print(talaba1.get_info())
 
 
-class Fan:
-    def __init__(self,nomi):
-        self.nomi = nomi
-        self.talabalar_soni = 0
-        self.talabalar = []
+# class Fan:
+#     def __init__(self,nomi):
+#         self.nomi = nomi
+#         self.talabalar_soni = 0
+#         self.talabalar = []
     
-    def add_student(self, talaba):
-        self.talabalar.append(talaba)
-        self.talabalar_soni += 1
+#     def add_student(self, talaba):
+#         self.talabalar.append(talaba)
+#         self.talabalar_soni += 1
     
-    def get_name(self):
-        return self.nomi
+#     def get_name(self):
+#         return self.nomi
     
-    def get_students(self):
-        return [x.get_info() for x in self.talabalar]
+#     def get_students(self):
+#         return [x.get_info() for x in self.talabalar]
       
-    def get_students_num(self):
-        """Fanga yozilgan talabalar soni"""
-        return self.talabalar_soni
+#     def get_students_num(self):
+#         """Fanga yozilgan talabalar soni"""
+#         return self.talabalar_soni
  
-matematika = Fan("Oliy Matematika")
-english = Fan("English")
+# matematika = Fan("Oliy Matematika")
+# english = Fan("English")
 
 # talaba1 = Talaba("Alijon","Valiyev",2000)
 # talaba2 = Talaba("Hasan","Alimov",2001)
@@ -91,41 +91,41 @@ english = Fan("English")
 # print(talaba1.__dict__.keys())
 # print(talaba1.__dict__.values())
 
-class Sinf:
-    def __init__(self, nomi, kurs, raxbar, xona):
-        self.nomi = nomi
-        self.kurs = kurs
-        self.talabalar = []
-        self.talabalar_soni = 0
-        self.raxbar = raxbar
-        self.xona = xona
-        self.fanlar = []
+# class Sinf:
+#     def __init__(self, nomi, kurs, raxbar, xona):
+#         self.nomi = nomi
+#         self.kurs = kurs
+#         self.talabalar = []
+#         self.talabalar_soni = 0
+#         self.raxbar = raxbar
+#         self.xona = xona
+#         self.fanlar = []
         
-    def add_student(self, talaba):
-        self.talabalar.append(talaba)
-        self.talabalar_soni += 1
+#     def add_student(self, talaba):
+#         self.talabalar.append(talaba)
+#         self.talabalar_soni += 1
     
-    def get_name(self):
-        return self.nomi
+#     def get_name(self):
+#         return self.nomi
     
-    def get_students(self):
-        return [x.get_info() for x in self.talabalar]
+#     def get_students(self):
+#         return [x.get_info() for x in self.talabalar]
       
-    def get_students_num(self):
-        """Fanga yozilgan talabalar soni"""
-        return self.talabalar_soni
+#     def get_students_num(self):
+#         """Fanga yozilgan talabalar soni"""
+#         return self.talabalar_soni
     
         
 
-talaba1 = Talaba("Aziz","Valiyev",2000)
-talaba2 = Talaba("Isfandiyor","Alimov",2001)
-talaba3 = Talaba("Jamshid","Boriyev",2001)
+# talaba1 = Talaba("Aziz","Valiyev",2000)
+# talaba2 = Talaba("Isfandiyor","Alimov",2001)
+# talaba3 = Talaba("Jamshid","Boriyev",2001)
         
-sinf_10_i = Sinf('I', 10, 'KKK', 12)
+# sinf_10_i = Sinf('I', 10, 'KKK', 12)
 
-sinf_10_i.add_student(talaba1)
-sinf_10_i.add_student(talaba2)
-sinf_10_i.add_student(talaba3)
+# sinf_10_i.add_student(talaba1)
+# sinf_10_i.add_student(talaba2)
+# sinf_10_i.add_student(talaba3)
 
 
 
