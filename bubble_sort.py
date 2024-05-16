9e4b035b4eb467a0d2a0db5681258f801e5a3870
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-05-16 15:40:56 +0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,18 @@
 nums = [5, 7, 2, 10, 0, 4, 9, 8, 6, 1, 3]
 
+
 def bubble(nums):
     n = len(nums)
-    for i in range(n-1):
-        if nums[i] >= nums[i+1]:
-            nums[i], nums[i+1] = nums[i+1], nums[i]
+    for j in range(n-1):
+        next_ = False
+        for i in range(n-j-1):
+            if nums[i] >= nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                next_ = True
+                
+        if not next_:
+            break
     return nums
           
-
 print(bubble(nums))
+
