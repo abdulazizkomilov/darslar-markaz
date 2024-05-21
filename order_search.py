fc1267e1395731ac489481974187d30873b86910
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-05-21 09:51:52 +0500

    Update order_search.py

--- order_search.py
+++ order_search.py
@@ -2,23 +2,23 @@ array = [4, 6, 1, 9]
 
 def func(arr):
     for i in range(0, len(arr)):
-        for _ in range(i+1, len(arr)):
-            if arr[i] >= arr[_]:
-                arr[i], arr[_] = arr[_], arr[i]
+        for j in range(i+1, len(arr)):
+            if arr[i] >= arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
     return arr
 
-# print(func(array))
+print(func(array))
 
 
 
 
-list_1 = [1,2,3,4,5,6,8,9,10,12]
+# list_1 = [1,2,3,4,5,6,8,9,10,12]
 
-def find(nums: list[int]) -> int:
-    N = len(nums)+1
-    sumE = N*(N+1)/2
-    sumA = sum(nums)
+# def find(nums: list[int]) -> int:
+#     N = len(nums)+1
+#     sumE = N*(N+1)/2
+#     sumA = sum(nums)
     
-    return sumE-sumA
+#     return sumE-sumA
 
-print(find(list_1))
+# print(find(list_1))
