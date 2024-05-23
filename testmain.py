11bef66f4f02a539e470db0a1d8908a4b655b5fd
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-05-23 06:34:52 +0500

    Create testmain.py

---
+++ testmain.py
@@ -0,0 +1,36 @@
+# ----- 1 -----
+# def main() -> None:
+#     print()
+#     MyClass = type("", (), {})
+#     my_obj = MyClass()
+#     print(type(my_obj))
+
+# main()
+
+
+# ----- 2 -----
+# some_str = "Python"
+# some_dict = {}
+
+# for i, some_dict[i] in enumerate(some_str):
+#     i = 1
+    
+# print(some_dict)
+
+
+# ----- 3 -----
+# def generator_x():
+#     num = 1
+#     while num < 5:
+#         if num % 2:
+#             yield num
+#         num += 1
+        
+# # print(next(generator_x()))
+
+# for value in generator_x():
+#     print(value)
+ 
+
+# ----- 4 -----
+
